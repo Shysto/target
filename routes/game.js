dddd4750d6d79920ed533f069ab8c1cd796a221b
Nathan Boyer <nathan.boyer97@gmail.com>
2019-06-25 15:12:51 +0200

    :lipstick: Update game.js style

--- routes/game.js
+++ routes/game.js
@@ -3,7 +3,7 @@ var express= require('express');
 var router = express.Router();
 var {ensureAuthenticated} = require('../libs/myLibUtils');
 
-//effectue une requête get, si l'utilisateur est loggé, on affiche la vue index sinon, on affiche la vue "game" -> A CHANGER
+//Call game view if user is connected
 router.get('/',ensureAuthenticated, function(req,res){
     res.render('game');
 });
