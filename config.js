d25e158282d3bb8547198fcc2aa7f15da33d2be6
TanguyNavez <50314633+TanguyNavez@users.noreply.github.com>
2019-06-26 10:59:10 +0200

    :lipstick: Delete the upper bar and style the lower bar

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root",
-  port: "8889",
+  password: "",
+  port: "",
   database: "target"
 });
 
