a2f38c00f9b795b0269b17a9d4a29f1025735256
Wall-e <leguillou.eve@gmail.com>
2019-06-28 13:24:56 +0200

    :ambulance: Fix a bug

--- config.js
+++ config.js
@@ -3,9 +3,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
 
     host: "localhost",
-    user: "root",
-    password: "root",
-    port: "8889",
+    user: "web",
+    password: "motdepasse",
     database: "target"
 });
 
